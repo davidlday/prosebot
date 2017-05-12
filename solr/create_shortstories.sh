16ae43f7f84d6437b046bc211038aa61e022e373
David L. Day <dday376@gmail.com>
2017-05-12 15:55:29 -0400

    Created separate directories for scrapy and solr configs.

---
+++ solr/create_shortstories.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Make sure solr is started
+$SOLR_ROOT/bin/solr start -p 8983
+
+# Create core and import schema
+$SOLR_ROOT/bin/solr create -c shortstories
+curl -X POST -H 'Content-type:application/json' --data-binary @schema.json http://localhost:8983/solr/shortstories/schema
+
+# Restart solr
+# See: https://stackoverflow.com/questions/29445323/adding-a-document-to-the-index-in-solr-document-contains-at-least-one-immense-t#31720634
+$SOLR_ROOT/bin/solr stop -p 8983
+$SOLR_ROOT/bin/solr start -p 8983
