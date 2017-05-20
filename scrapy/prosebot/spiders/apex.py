39a8a8072c9d5a31e67ccf0a327d10a3794b8e89
David L. Day <dday376@gmail.com>
2017-05-20 10:09:27 -0400

    Added Apex Magazine.

---
+++ scrapy/prosebot/spiders/apex.py
@@ -0,0 +1,75 @@
+import scrapy
+import calendar
+import re
+from prosebot.items import Story
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class ApexSpider(CrawlSpider):
+    name            = 'apex'
+    allowed_domains = ['apex-magazine.com']
+    start_urls      = ['http://www.apex-magazine.com/category/short-fiction/']
+    content_xpath   = '//*[@id="left-area"]'
+    rules           = (
+        # Fiction index pages.
+        Rule(LinkExtractor(
+                allow=([
+                    '/category/short-fiction/',
+                    '/category/short-fiction/page/\d{1,}/'
+                ]),
+                restrict_xpaths=(content_xpath)
+            )
+        ),
+        # Fiction stories.
+        Rule(LinkExtractor(
+                deny=(
+                    '/category/short-fiction/',
+                    '/category/short-fiction/page/\d{1,}/'
+                    '/clavis-aurea/',
+                    '/issue-.*',
+                    '/backissues/',
+                    '/back-issues-.*',
+                    '/support-us',
+                    '/apex-magazine-podcast/',
+                    '/about-apex-magazine/',
+                    '/digital-reader-apps/',
+                    '/masthead-2/',
+                    '/submission-guidelines/',
+                    '/advertising/',
+                    '/our-authors/',
+                    '/awards-and-nominations/',
+                    '/contact-2/',
+                    '/story-of-the-year/',
+                    '/category/.*',
+                    '/tag/.*',
+                    '/author/.*',
+                    '/feed/',
+                ),
+                restrict_xpaths=(content_xpath)
+            ), callback='parse_story'),
+        )
+
+    def parse_story(self, response):
+        base_xpath      = '//*[@id="content-area"]'
+        story_post      = response.xpath(base_xpath)
+        text            = ''
+
+        story           = Story()
+        story['magazine'] = 'Apex Magazine'
+        story['genre']  = ['fantasy', 'horror', 'science fiction']
+        story['url']    = response.url
+        story['original_tags'] = story_post.xpath('.//div[@id="tags"]/a/text()').extract()
+        story['title']  = story_post.xpath('.//*[@id="category-name"]/h1[@class="category-title"]/text()').extract()[0]
+        story['author'] = story_post.xpath('.//*[@id="category-name"]/p[@class="description"]/a[@rel="author"]/text()').extract()
+
+        # Extract Published Month / Year
+        story['pub_date'] = response.xpath('.//meta[@property="article:published_time"]/@content').extract()[0].split('T')[0]
+        [story['pub_year'], pub_month_no, pub_day] = story['pub_date'].split('-')
+        story['pub_month'] = calendar.month_name[int(pub_month_no)].lower()
+
+        # Extract the body of the story
+        story_lines = [p.strip() for p in story_post.xpath('.//*[@id="left-area"]/div[contains(@class,"post")]/p//text()').extract()]
+        story['text']   = "\n".join(story_lines)
+
+        yield story
