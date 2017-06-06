86e9524f24774116fee8d1dd31b8cf653e7dba3c
David L. Day <dday376@gmail.com>
2017-06-05 20:56:29 -0400

    Closer to getting dates for Uncanny, but not there yet.

--- scrapy/prosebot/spiders/uncanny.py
+++ scrapy/prosebot/spiders/uncanny.py
@@ -14,37 +14,10 @@ class UncannySpider(CrawlSpider):
     magazine_genre  = ['fantasy','science fiction']
     allowed_domains = ['uncannymagazine.com']
     start_urls      = [
-        'http://uncannymagazine.com/issues/',
         'http://uncannymagazine.com/type/fiction/',
     ]
     content_xpath   = '//main[@class="archive_main"]'
     rules           = (
-        # Issue index pages.
-        Rule(LinkExtractor(
-                allow=([
-                    '/issues/',
-                    '/issues/page/\d{1,}/',
-                ]),
-                restrict_xpaths=(content_xpath)
-            )
-        ),
-        # Issue page.
-        Rule(LinkExtractor(
-                allow=([
-                    '/issues/.+/',
-                ]),
-                deny=([
-                    '/type/fiction/',
-                    '/type/fiction/page/\d{1,}/',
-                    '/authors/.*',
-                    '/nonfiction/.*',
-                    '/articles/.*',
-                    '/ebooks/.*',
-                    '/subscribe/.*',
-                ]),
-                restrict_xpaths=(content_xpath)
-            ), callback='parse_issue_date'
-        ),
         # Fiction index pages.
         Rule(LinkExtractor(
                 allow=([
@@ -78,15 +51,26 @@ class UncannySpider(CrawlSpider):
 
     # Uncanny doesn't provide dates for each story, but does link to the issue.
     # Build a list of issue URLs and corresponding publication date.
+    # See: https://doc.scrapy.org/en/latest/topics/request-response.html#passing-additional-data-to-callback-functions
     def parse_issue_date(self, response):
+        story = response.meta['story']
+        print("Looking for issue date: %s" % response.url)
         if response.url not in self.issue_dates:
-            date_text = response.xpath(
-                '//main[@class="issue_main"]/article/div[@class="issue_content"]/p/text()'
-            ).extract()
+            date_text = [' '.join(p.xpath('.//text()').extract())
+                for p in response.xpath(
+                    '//main[@class="issue_main"]//div[@class="issue_content"]/p'
+                )]
             match = self.issue_date_pattern.match(date_text[0])
-            if match:
-                self.issue_dates[response.url] = dateutil.parser.parse(match[0].strip())
-                print(response.url + ':' + self.issue_dates[response.url])
+            self.issue_dates[response.url] = dateutil.parser.parse(match[0].strip())
+            print(response.url + ': ' + self.issue_dates[response.url])
+        # Set date based on issue
+        meta_pub_date = self.issue_dates[response.url]
+        story['pub_year'] = str(meta_pub_date.year)
+        pub_month_no = "%02d" % meta_pub_date.month
+        story['pub_month'] = calendar.month_name[meta_pub_date.month].lower()
+        story['pub_date'] = story['pub_year'] + '-' + pub_month_no + '-01'
+
+        yield story
 
     def parse_story(self, response):
         base_xpath      = '//main[@class="main"]/article[re:test(@id,"post-\d+")]'
@@ -102,20 +86,20 @@ class UncannySpider(CrawlSpider):
         story['title']  = ' '.join(story_post.xpath('./h2[contains(@class,"entry-title")]//text()').extract())
         story['author'] = story_post.xpath('./h4[@class="byline"]/a/text()').extract()
 
-        # Derive publication date from issue url
-        issue_link = response.xpath(
-            '//aside/div[@class="widget"]/div[@class="featured_issue_thumbnail"]/a/@href'
-        ).extract()[0]
-        meta_pub_date = self.issue_dates[issue_link]
-        story['pub_year'] = str(meta_pub_date.year)
-        pub_month_no = "%02d" % meta_pub_date.month
-        story['pub_month'] = calendar.month_name[meta_pub_date.month].lower()
-        story['pub_date'] = story['pub_year'] + '-' + pub_month_no + '-01'
-
         # Extract the body of the story
         story_lines = [''.join(p.xpath('.//text()').extract())
                 for p in story_post.xpath('.//div[contains(@class, "entry-content")]/p[re:test(@class,"p[1-3]")]')]
         story['text']   = "\n".join(story_lines)
 
-        yield story
+        # Derive publication date from issue page
+        # See: https://stackoverflow.com/questions/33589136/scrapy-understanding-how-do-items-and-requests-work-between-callbacks#33590207
+        issue_url = response.xpath(
+            '//aside/div[@class="widget"]/div[@class="featured_issue_thumbnail"]/a/@href'
+        ).extract()[0]
+        issue_request = scrapy.Request(
+            issue_url, callback=self.parse_issue_date
+        )
+        issue_request.meta['story'] = story
+
+        yield issue_request
 
